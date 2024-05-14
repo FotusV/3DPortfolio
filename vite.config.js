6337334ffd11d3347b8e8c9f9310e4ed0999f827
Kyle Winch <kyle5cents@gmail.com>
2024-05-13 21:45:55 -0400

    Updated build configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/3DPortfolio/',  // Set the base URL for GitHub Pages
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',  // Set the output directory for the build files
+    rollupOptions: {
+      // Optionally configure Rollup if needed
+    }
+  }
+});
